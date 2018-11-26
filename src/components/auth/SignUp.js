f8d3d6ad2b916d8ba736bccad7f3b7efd8d655a3
gurungprakas <gooroongprakas@gmail.com>
2018-11-26 14:13:38 +0545

    commit after route guarding

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class SignUp extends Component {
     state = {
@@ -20,6 +22,8 @@ class SignUp extends Component {
         console.log(this.state);
     }
   render() {
+    const { auth } = this.props;
+      if(auth.uid) return < Redirect to='/' />
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -49,5 +53,10 @@ class SignUp extends Component {
     )
   }
 }
+const mapStateToProps = (state) => {
+    return{
+        auth: state.firebase.auth
+    }
+}
 
-export default SignUp;
+export default connect(mapStateToProps)(SignUp);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
@@ -16,9 +16,14 @@ const store = createStore(rootReducer,
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
     )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    // serviceWorker.unregister();
+    registerServiceWorker();
+
+})
+
